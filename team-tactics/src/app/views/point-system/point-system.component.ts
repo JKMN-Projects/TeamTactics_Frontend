6332e9f772a51abf6aace6c6065a8c8422b96a12
MP-Hartvig <mip@hartvigconsult.dk>
2025-02-27 13:29:37 +0100

    Added dockerfile, several components and folders for structure.

---
+++ team-tactics/src/app/views/point-system/point-system.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-point-system',
+  standalone: true,
+  imports: [],
+  templateUrl: './point-system.component.html',
+  styleUrl: './point-system.component.css'
+})
+export class PointSystemComponent {
+
+}
